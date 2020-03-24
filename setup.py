9c3d5801d215194fb736e4d930eeb46198995e42
Sri Gowrishankar <sri.gowrishankar@thp.ca>
2020-03-24 15:24:25 -0400

    COVID19-Analysis: Intialize cookiecutter for datascience

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Collaboration space for all analytics related to the Ontario COVID19 analysis project',
+    author='Ontario-COVID19-Analysis',
+    license='MIT',
+)
